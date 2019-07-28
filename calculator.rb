a5319a45975fd1ed7fe161232a1785959bdf98cd
Katie Wokasch <katie.wokasch@gmail.com>
2019-07-28 20:10:00 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -5,27 +5,6 @@ describe "./calculator.rb" do
     #first_number needs to be an integer or a float 
     if get_variable_from_file = Integer || Float
   end
+  
 
-  second_number = get_variable_from_file ('./calculator.rb', "second_number")
-    #second_number needs to be an integer or a float and cannot equal zero 
-    if get_variable_from_file = Integer || Float && 
-      get_variable_from_file != 0 
-  end
   
-  
-  def sum (first_number, second_number)
-    first_number + second_number
-  end
-  
-  def difference (first_number, second_number)
-     first_number - second_number
-  end
-  
-  def product (first_number, second_number)
-    first_number * second_number
-  end
-
-  def quotient (first_number, second_number) 
-    first_number / second_number
-  end 
-end
