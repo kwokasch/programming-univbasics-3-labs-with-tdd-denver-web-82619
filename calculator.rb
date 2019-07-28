d54e418d1232336ffa78441a5f1740507117ddaa
Katie Wokasch <katie.wokasch@gmail.com>
2019-07-28 20:12:26 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,11 +2,7 @@
 describe "./calculator.rb" do
 
   first_number = get_variable_from_file ('./calculator.rb', "first_number")
-    #first_number needs to be an integer or a float 
-    if get_variable_from_file = Integer || Float
-      puts first_number
-    else
-      puts nil
+   
   end
   
 end 
