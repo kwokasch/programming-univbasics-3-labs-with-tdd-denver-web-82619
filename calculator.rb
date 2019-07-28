cd16472312cbb587ea98458643bd8b316600c5cd
Katie Wokasch <katie.wokasch@gmail.com>
2019-07-28 20:24:19 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -6,12 +6,7 @@ def describe "./calculator.rb" do
   second_number = get_variable_from_file ('./calculator.rb', "second_number")
   
   sum (first_number, second_number) 
-    puts #{first_number} + #{second_number}
+    puts first_number + second_number
   end
-  
-  
-  difference = first_number - second_number
-end 
-  
 
   
