a833b52e805346dc2d7eb0d8ee0a36791c3d784a
Katie Wokasch <katie.wokasch@gmail.com>
2019-07-28 20:14:59 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,10 +1,11 @@
 
 describe "./calculator.rb" do
 
-  #first_number = get_variable_from_file ('./calculator.rb', "first_number")
-  first_number = 10 
+  first_number = get_variable_from_file ('./calculator.rb', "first_number") 
   
-  second_number = 10 
+  second_number = get_variable_from_file ('./calculator.rb', "second_number")
+  
+
   
 end 
   
